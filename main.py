d61209598d4eda667b74b8507cb7e3086303331e
Luu Khang Huy <luuhuy.290303@gmail.com>
2026-08-10 11:01:48 +0700

    Change default local port to 8888 to prevent port 8080 binding conflicts

--- main.py
+++ main.py
@@ -415,6 +415,7 @@ async def submit_to_google_form(request: SubmitRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8888))
     print(f"Khởi chạy Server tại cổng {port}...")
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+
